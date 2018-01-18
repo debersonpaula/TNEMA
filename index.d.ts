7cef0f84d2a660723111969dfa469e65c554f5bb
Deberson de Paula <31399556+debersonpaula@users.noreply.github.com>
2018-01-18 09:14:00 -0200

    Release 0.3.8
    Correction of definition

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@
  * Copyright(c) 2017 Deberson Paula
  * MIT Licensed
  */
-import { TNEMAServer } from './lib/main';
-export { TNEMAServer };
+import { TNEMAServer, TModel, TSchema } from './lib/main';
+export { TNEMAServer, TModel, TSchema };
